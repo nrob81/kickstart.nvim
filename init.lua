cb12c588c7bd23a6a6dd15f8648b4aed524266f7
Robert Natkay <natkay.robert@nrcode.com>
2025-01-01 00:29:06 +0100

    install gruvbox, lsp_signature, ts-context, outline

--- init.lua
+++ init.lua
@@ -840,13 +840,14 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'gruvbox'
+      vim.opt.bg = 'light'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -917,6 +918,28 @@ require('lazy').setup({
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  },
+
+  { 'nvim-treesitter/nvim-treesitter-context' },
+
+  {
+    'ray-x/lsp_signature.nvim',
+    event = 'BufRead',
+    config = function()
+      require('lsp_signature').on_attach {
+        bind = true,
+        floating_window = true,
+        -- hint_prefix = "🗨️",
+      }
+    end,
+  },
+
+  {
+    'hedyhli/outline.nvim',
+    config = function()
+      vim.keymap.set('n', '<leader>o', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
+      require('outline').setup {}
+    end,
   },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
