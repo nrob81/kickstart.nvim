b883e1cfc4122754a172f866ff1dc8aa7db13fcc
Robert Natkay <robert.natkay@furbify.eu>
2025-01-03 10:27:10 +0100

    fix intelephense

--- lua/custom/plugins/intelephense.lua
+++ lua/custom/plugins/intelephense.lua
@@ -1,12 +1,12 @@
+-- lua/custom/plugins/intelephense.lua
 return {
   'neovim/nvim-lspconfig',
   event = { 'BufReadPre', 'BufNewFile' },
   cmd = { 'LspInfo', 'LspInstall', 'LspUninstall' },
   config = function()
-    -- Get the lspconfig module
     local lspconfig = require 'lspconfig'
+    local telescope_builtin = require 'telescope.builtin'
 
-    -- Configure Intelephense
     lspconfig.intelephense.setup {
       settings = {
         intelephense = {
@@ -42,6 +42,7 @@ return {
             'pdo',
             'pgsql',
             'phar',
+            'rdkafka',
             'readline',
             'recode',
             'reflection',
@@ -69,30 +70,67 @@ return {
           },
           files = {
             maxSize = 5000000,
+            associations = { '*.php', '*.phtml', '*.inc', '.php', '*.module', '*.theme' },
+            exclude = {
+              '**/.git/**',
+              '**/.svn/**',
+              '**/.hg/**',
+              '**/CVS/**',
+              '**/.DS_Store/**',
+              '**/node_modules/**',
+              '**/bower_components/**',
+              '**/vendor/**/{Tests,tests}/**',
+            },
           },
         },
       },
       on_attach = function(client, bufnr)
-        -- Define keybindings when LSP attaches to a buffer
-        local opts = { buffer = bufnr, noremap = true, silent = true }
+        -- Helper function for setting keymaps
+        local map = function(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, {
+            buffer = bufnr,
+            noremap = true,
+            silent = true,
+            desc = desc,
+          })
+        end
 
-        vim.keymap.set('n', '<leader>lh', vim.lsp.buf.hover, vim.tbl_extend('force', opts, { desc = 'PHP: Show Documentation' }))
+        -- Standard LSP keybindings from kickstart
+        map('n', 'gd', telescope_builtin.lsp_definitions, 'LSP: Go to Definition')
+        map('n', 'gr', telescope_builtin.lsp_references, 'LSP: Go to References')
+        map('n', 'gI', telescope_builtin.lsp_implementations, 'LSP: Go to Implementation')
+        map('n', '<leader>D', telescope_builtin.lsp_type_definitions, 'LSP: Type Definition')
+        map('n', '<leader>ds', telescope_builtin.lsp_document_symbols, 'LSP: Document Symbols')
+        map('n', '<leader>ws', telescope_builtin.lsp_dynamic_workspace_symbols, 'LSP: Workspace Symbols')
+        map('n', 'K', vim.lsp.buf.hover, 'LSP: Hover Documentation')
+        map('n', 'gD', vim.lsp.buf.declaration, 'LSP: Go to Declaration')
 
-        vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, vim.tbl_extend('force', opts, { desc = 'PHP: Go to Implementation' }))
+        -- PHP specific keybindings
+        map('n', '<leader>lh', vim.lsp.buf.hover, 'PHP: Show Documentation')
+        map('n', '<leader>li', vim.lsp.buf.implementation, 'PHP: Go to Implementation')
+        map('n', '<leader>lr', vim.lsp.buf.references, 'PHP: Find References')
+        map('n', '<leader>ls', vim.lsp.buf.signature_help, 'PHP: Show Signature')
+        map('n', '<leader>lf', vim.lsp.buf.format, 'PHP: Format')
+        map('n', '<leader>lo', '<cmd>Outline<cr>', 'PHP: Toggle Outline')
+        map('n', '<leader>ln', vim.lsp.buf.rename, 'PHP: Rename Symbol')
+        map('n', '<leader>la', vim.lsp.buf.code_action, 'PHP: Code Action')
 
-        vim.keymap.set('n', '<leader>lr', vim.lsp.buf.references, vim.tbl_extend('force', opts, { desc = 'PHP: Find References' }))
+        -- Document highlight on hover
+        if client.supports_method 'textDocument/documentHighlight' then
+          local group = vim.api.nvim_create_augroup('lsp_document_highlight', { clear = true })
+          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+            buffer = bufnr,
+            group = group,
+            callback = vim.lsp.buf.document_highlight,
+          })
+          vim.api.nvim_create_autocmd('CursorMoved', {
+            buffer = bufnr,
+            group = group,
+            callback = vim.lsp.buf.clear_references,
+          })
+        end
 
-        vim.keymap.set('n', '<leader>ls', vim.lsp.buf.signature_help, vim.tbl_extend('force', opts, { desc = 'PHP: Show Signature' }))
-
-        vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, vim.tbl_extend('force', opts, { desc = 'PHP: Format' }))
-
-        vim.keymap.set('n', '<leader>lo', '<cmd>Outline<cr>', vim.tbl_extend('force', opts, { desc = 'PHP: Toggle Outline' }))
-
-        vim.keymap.set('n', '<leader>ln', vim.lsp.buf.rename, vim.tbl_extend('force', opts, { desc = 'PHP: Rename Symbol' }))
-
-        vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, vim.tbl_extend('force', opts, { desc = 'PHP: Code Action' }))
-
-        -- Print a message when LSP attaches
+        -- Print debug message
         print('Intelephense attached to buffer', bufnr)
       end,
     }
@@ -101,5 +139,6 @@ return {
     'hrsh7th/cmp-nvim-lsp',
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
+    'nvim-telescope/telescope.nvim',
   },
 }
